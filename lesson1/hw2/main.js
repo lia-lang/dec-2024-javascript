2946c3c253f08a64e8c46c2f7bf4c239a60a95b9
lia-lang <yurchenkoyuliya2001@gmail.com>
2025-02-07 19:17:48 +0100

    first commit

---
+++ lesson1/hw2/main.js
@@ -0,0 +1,7 @@
+let firstName = 'Юлія';
+let middleName = 'Юріївна';
+let lastName = 'Юрченко';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+
+console.log(person);
---
+++ lesson1/hw4/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Ваше ім'я:");
+let middleName = prompt("Ваше ім'я по-батькові:");
+let age = prompt("Скільки вам років:");
+
+console.log("Ім'я: " + firstName);
+console.log("По-Батькові: " + middleName);
+console.log("Роки: " + age);
