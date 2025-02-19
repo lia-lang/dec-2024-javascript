edb625f55c09c5afe8a7e6b71596b325426c502c
lia-lang <yurchenkoyuliya2001@gmail.com>
2025-02-19 19:29:18 +0100

    next commit

---
+++ lesson8/main.js
@@ -0,0 +1,38 @@
+// hw1
+function cloner(obj) {
+    if (obj) {
+        let functions = [];
+        for (const key in obj) {
+            if (typeof obj[key] === 'function') {
+                const functionClone = obj[key].bind({});
+                functions.push({functionClone, key});
+            }
+        }
+        const cloneObj = JSON.parse(JSON.stringify(obj));
+        for (const func of functions) {
+            cloneObj[func.key] = func.functionClone;
+        }
+
+        console.log(cloneObj);
+        return cloneObj
+
+    }
+
+    throw new Error('!!!!!');
+
+}
+
+const clone = cloner({id: 123, name: 'asdsad', greeting() {console.log('hello')}, foo() {console.log('bar')}});
+clone.foo();
+
+// hw2
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.map((course, index) => ({...course, id: index + 1})));
